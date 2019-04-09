5ecf233d4595a9ba44cb097f2ecfcff2764b7810
SarBH <sbalayla@stevens.edu>
2019-04-09 13:13:58 -0400

    Update coffeemachine.py
    User can do any of the 4 action at any time:
    1. add water
    2. insert capsule
    3. press Brew button
    4. press Power button
    
    The respective methods take care of what happens if each one of the above is called from each state

--- coffeemachine.py
+++ coffeemachine.py
@@ -1,36 +1,58 @@
 
-def pressPower():
-    print("Power Button Pressed, your brewing machine is on")
+def pressPower(powerOn):
+    print("Power Button Pressed")
+    if powerOn:
+        print("Machine has been turned OFF")
+        powerOn = False
+    else: 
+        print("Machine has been turned ON")
+        powerOn = True
+    return powerOn
+    
+    
 
-def addWater(waterin, capsulein):
-    if waterin == True:
-        print("machine already has water")
+def addWater(waterin, capsulein, powerOn):
+    if powerOn:
+        if waterin == True:
+            print("machine already has water")
+        else:
+            if capsulein == True: 
+                print("Adding water...Capsule and water in. Ready to brew")
+            elif capsulein == False:
+                print("Adding water...Water in. Insert capsule to brew")
     else:
-        if capsulein == True: 
-            print("Capsule and water in. Ready to brew")
-        elif capsulein == False:
-            print("Water in. Insert capsule to brew")
+        print("--doing nothing because power is off--\n")
     waterin = True
     return waterin
 
 
-def insertCapsule(waterin, capsulein):
-    if waterin == True:
-        print("Capsule and water in. Ready to brew")
-    if waterin == False:
-        print("Capsule in. Add water to brew")
-    capsulein = True
+def insertCapsule(waterin, capsulein, powerOn):
+    if powerOn:
+        if capsulein == True:
+            print("machine already has a capsule in")
+        else:
+            if waterin == True: 
+                print("Adding capsule...Capsule and water in. Ready to brew")
+            elif capsulein == False:
+                print("Adding Capsule...Capsule in. Add water to brew")
+    else:
+        print("--doing nothing because power is off--\n")
+    waterin = True
+    return waterin
 
 
-def pressBrew(waterin, capsulein):
-    if waterin and capsulein:
-        print("Water=1/Capsule=1, action = pressBrew, Final State: )
-        print("brewing in progress")
-        wait30()
-    elif waterin and not capsulein:
-        print("insert capsule first")
-    elif capsulein and not waterin:
-        print("add water first")
+def pressBrew(waterin, capsulein, powerOn):
+    if powerOn:    
+        if waterin and capsulein:
+            print("brewing in progress")
+        elif waterin:
+            print("insert capsule first")
+        elif capsulein:
+            print("add water first")
+        else:
+            print("insert capsule and add water before brewing")
+    else:
+        print("--doing nothing because power is off--\n")
 
 def wait30():
     print("you are waiting for the brewing to finish")
@@ -42,26 +64,21 @@ def wait300():
 def main():
     waterin = False
     capsulein = False
-    pressPower()
-
-    while not waterin and not capsulein:
-        action1 = input("what do you want to do? Press 1 to add water, press 2 to insert capsule\n")
-        if action1 == '1':
-            addWater(waterin, capsulein)
-        elif action1 == '2':
-            insertCapsule(waterin, capsulein)
-        # action2 = input("what do you want to do? Press 1 to add water, press 2 to insert capsule\n")
-        # if action2 == '1':
-        #     addWater(waterin, capsulein)
-        # elif action2 == '2':
-        #     insertCapsule(waterin, capsulein)
+    powerOn = False
     
-    startBrew = input("type 'brew' to brew...\n")
-    
-    if startBrew == 'brew':
-        pressBrew(waterin, capsulein)
-    else:
-        print("I am sorry.. that is not a proper request")
+    # while not waterin and not capsulein:
+    while True:
+        action1 = input("what do you want to do? Enter 'water' to add water, 'capsule' to add coffee capsule, 'power' to press power button or 'brew' to press brew button\n")
+        if action1 == 'water':
+            waterin = addWater(waterin, capsulein, powerOn)
+        elif action1 == 'capsule':
+            capsulein = insertCapsule(waterin, capsulein, powerOn)
+        elif action1 == 'power':
+            powerOn = pressPower(powerOn)
+        elif action1 == 'brew':
+            pressBrew(waterin, capsulein, powerOn)
+        else:
+            print("I am sorry.. that is not a proper request")
 
     wait30()
     wait300()
